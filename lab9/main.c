93b4253caa6d2170a9be3fc96da875e68743670b
mentalMint <walkershush@gmail.com>
2022-10-12 15:34:58 +0700

    Make lab9 incorrectly

--- lab9/main.c
+++ lab9/main.c
@@ -4,24 +4,58 @@
 #include <string.h>
 #include <limits.h>
 #include <signal.h>
-
-#define STEPS_NUM 100
+#include <unistd.h>
 
 long int threads_count;
+pthread_t* threads;
+struct thread_data* threads_data;
 
-void termination_handler(int signum) {
+struct thread_data {
+    int thread_number;
+    double result;
+};
 
-}
-
-double* calculate_pi_part(const int* thread_number) {
-    double* pi_part = (double*) malloc(sizeof(double));
-    for (int i = 0; i < STEPS_NUM; i++) {
-        *pi_part += 1.0 / ((i * (int) threads_count + *thread_number) * 4.0 + 1.0);
-        *pi_part -= 1.0 / ((i * (int) threads_count + *thread_number) * 4.0 + 3.0);
+void termination_handler() {
+    for (int i = 0; i < threads_count; i++) {
+        int return_value = pthread_cancel(threads[i]);
+        if (0 != return_value) {
+            fprintf(stderr, "Error while canceling the thread: %s\n", strerror(return_value));
+            pthread_exit((void*) EXIT_FAILURE);
+        }
     }
 
-    printf("Thread %d Pi_part / 4 = %.15lf \n", *thread_number, *pi_part);
-    return pi_part;
+    double pi_div_by_4 = 0;
+    for (int i = 0; i < threads_count; i++) {
+        int return_value = pthread_join(threads[i], NULL);
+        if (0 != return_value) {
+            fprintf(stderr, "Cannot join a thread: %s", strerror(return_value));
+            pthread_exit((void*) EXIT_FAILURE);
+        }
+        pi_div_by_4 += threads_data[i].result;
+    }
+    printf("\nPi = %.15lf\n", pi_div_by_4 * 4);
+
+    free(threads_data);
+    free(threads);
+    pthread_exit((void*) EXIT_SUCCESS);
+}
+
+void calculate_pi_part(struct thread_data* data) {
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    if (0 != pthread_sigmask(SIG_BLOCK, &set, NULL)) {
+        perror("pthread_sigmask");
+        pthread_exit((void*) EXIT_FAILURE);
+    }
+
+    for (int i = 0;; i++) {
+        if (i % 10000000 == 0 && i != 0) {
+            pthread_testcancel();
+        }
+        data->result += 1.0 / ((i * (int) threads_count + data->thread_number) * 4.0 + 1.0);
+        data->result -= 1.0 / ((i * (int) threads_count + data->thread_number) * 4.0 + 3.0);
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -38,40 +72,25 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    pthread_t* threads = (pthread_t*) malloc(threads_count * sizeof(pthread_t));
-    int* threads_numbers = (int*) malloc(threads_count * sizeof(int));
+    threads = (pthread_t*) malloc(threads_count * sizeof(pthread_t));
+    threads_data = (struct thread_data*) malloc(threads_count * sizeof(struct thread_data));
     int return_value;
     for (int i = 0; i < threads_count; i++) {
-        threads_numbers[i] = i;
-        return_value = pthread_create(threads + i, NULL, (void*) calculate_pi_part, (void*) &threads_numbers[i]);
+        threads_data[i].thread_number = i;
+        return_value = pthread_create(threads + i, NULL, (void*) calculate_pi_part, (void*) &threads_data[i]);
         if (0 != return_value) {
-//            fprintf(stderr, "Cannot create a thread: %s", strerror(return_value));
+            fprintf(stderr, "Cannot create a thread: %s", strerror(return_value));
             pthread_exit((void*) EXIT_FAILURE);
         }
     }
 
     struct sigaction new_action;
-
-    /* Set up the structure to specify the new action. */
     new_action.sa_handler = termination_handler;
     sigemptyset(&new_action.sa_mask);
     new_action.sa_flags = 0;
-    sigaction (SIGINT, &new_action, NULL);
+    sigaction(SIGINT, &new_action, NULL);
 
-    double pi_div_by_4 = 0;
-    double* thread_return_value = NULL;
-    for (int i = 0; i < threads_count; i++) {
-        return_value = pthread_join(threads[i], (void**) &thread_return_value);
-        if (0 != return_value) {
-            fprintf(stderr, "Cannot join a thread: %s", strerror(return_value));
-            free(thread_return_value);
-            pthread_exit((void*) EXIT_FAILURE);
-        }
-        pi_div_by_4 += *thread_return_value;
-        free(thread_return_value);
-    }
-
-    printf("Pi = %.15lf\n", pi_div_by_4 * 4);
-    free(threads);
-    pthread_exit((void*) EXIT_SUCCESS);
+    pause();
+    perror("pause");
+    pthread_exit((void *)EXIT_FAILURE);
 }
